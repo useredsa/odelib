39f8cf0b80538b90b4575c97735ba533728ca827
useredsa <emilio.dominguezs@um.es>
2020-12-03 18:13:51 +0100

    Fixed step revision and multistep

--- include/methods/fixed_step.hpp
+++ include/methods/fixed_step.hpp
@@ -13,9 +13,19 @@ template<typename Derivative>
 struct Euler {
     Derivative f;
 
+    constexpr int order() {
+        return 1;
+    }
+
     template<int N>
-    inline Vectord<N> operator()(double t, const Vectord<N>& x, double step) {
-        return f(t, x)*step;
+    inline Vectord<N> step(double t, const Vectord<N>& x, double h) {
+        return hinted_step(t, x, h, f(t, x));
+    }
+
+    template<int N>
+    inline Vectord<N> hinted_step(double t, const Vectord<N>& x,
+                                  double h, const Vectord<N>& d) {
+        return d*h;
     }
 };
 
@@ -23,10 +33,19 @@ template<typename Derivative>
 struct ModEuler {
     Derivative f;
 
+    constexpr int order() {
+        return 2;
+    }
+
     template<int N>
-    inline Vectord<N> operator()(double t, const Vectord<N>& x, double step) {
-        Vectord<N> d = f(t, x)*step;
-        return (d + f(t+step, x+d)*step)/2;
+    inline Vectord<N> step(double t, const Vectord<N>& x, double h) {
+        return hinted_step(t, x, h, f(t, x));
+    }
+
+    template<int N>
+    inline Vectord<N> hinted_step(double t, const Vectord<N>& x,
+                                  double h, const Vectord<N>& d) {
+        return (d*h + f(t+h, x+d*h)*h)/2;
     }
 };
 
@@ -34,14 +53,24 @@ template<typename Derivative>
 struct RK4 {
     Derivative f;
 
+    constexpr int order() {
+        return 4;
+    }
+
     template<int N>
-    inline Vectord<N> operator()(double t, const Vectord<N>& x, double step) {
+    inline Vectord<N> step(double t, const Vectord<N>& x, double h) {
+        return hinted_step(t, x, h, f(t, x));
+    }
+
+    template<int N>
+    inline Vectord<N> hinted_step(double t, const Vectord<N>& x,
+                                  double h, const Vectord<N>& d) {
         Vectord<N> k[4];
-        k[0] = f(t, x)*step;
-        k[1] = f(t+step/2.0, x + k[0]/2.0)*step;
-        k[2] = f(t+step/2.0, x + k[1]/2.0)*step;
-        k[3] = f(t+step, x + k[2])*step;
-        return (k[0] + 2.0*k[1] + 2.0*k[2] + k[3])/6.0;
+        k[0] = d*h;
+        k[1] = f(t+h/2, x + k[0]/2)*h;
+        k[2] = f(t+h/2, x + k[1]/2)*h;
+        k[3] = f(t+h, x + k[2])*h;
+        return (k[0] + 2*k[1] + 2*k[2] + k[3])/6;
     }
 };
 
@@ -57,7 +86,7 @@ fixed_step_ode_solver(
     std::vector<Vectord<N>, Eigen::aligned_allocator<Vectord<N>>> x(iter+1);
     x[0] = x0;
     for (int i = 0; i < iter; ++i) {
-        x[i+1] = x[i] + met(t0, x[i], step);
+        x[i+1] = x[i] + met.step(t0, x[i], step);
         t0 += step;
     }
     return x;
---
+++ include/methods/multistep.hpp
@@ -0,0 +1,31 @@
+#ifndef INCLUDE_METHODS_MULTISTEP_HPP_
+#define INCLUDE_METHODS_MULTISTEP_HPP_
+
+#include "Vectord.hpp"
+
+namespace multistep {
+
+using Eigen::Vectord;
+
+template<typename Derivative>
+struct AdamsBashforth4 {
+    Derivative f;
+
+    constexpr int order() {
+        return 4;
+    }
+
+    constexpr int needed_steps() {
+        return 3;
+    }
+
+    template<int N>
+    inline Vectord<N> step(const Vectord<N>* x, const Vectord<N>* d, double h) {
+        return x[3] + h/24*(-9*d[0] + 37*d[1] - 59*d[2] + 55*d[3]);
+    }
+}  // namespace multistep
+
+}
+
+#endif // INCLUDE_METHODS_MULTISTEP_HPP_
+
